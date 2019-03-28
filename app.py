ae45af24879cd5bd4fc6802b31a8e195e7b1c00e
yangyang <yangeon@163.com>
2019-03-28 14:10:41 -0400

    https

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ model, sess, graph = init()
 
 testJson = json.dumps({"drawing":{"0":"[[[17, 174, 252, 255, 250, 250, 248, 244, 176, 136, 40, 15, 6, 3, 0, 5, 11], [4, 24, 25, 30, 48, 78, 92, 95, 92, 86, 86, 82, 79, 74, 15, 2, 0]], [[243, 243, 238, 219, 203, 17, 11, 3, 0, 0, 6, 10], [96, 157, 164, 154, 152, 156, 147, 143, 132, 94, 75, 75]], [[242, 229, 227, 215, 198, 179, 89, 42, 2, 0, 9, 12, 19], [165, 210, 214, 221, 220, 213, 213, 206, 204, 196, 177, 141, 136]], [[126, 120, 120, 132, 132, 123], [185, 192, 198, 197, 183, 183]]]"}})
 
-console.log("if there is a dresser printed below, means the model works")
+print("if there is a dresser printed below, means the model works")
 with graph.as_default():
     print(categories[perpareJSONDataAndPredict(model, testJson)[0]])
 
@@ -24,6 +24,9 @@ end = dt.datetime.now()
 print('{}, server initialized in .\nTotal time {}s'.format(end, (end - start).seconds))
 
 app = Flask(__name__)
+
+pathToCert = "/etc/letsencrypt/live/point99.xyz/cert.pem"
+pathToKey = "/etc/letsencrypt/live/point99.xyz/privkey.pem"
 
 def stringToRGB(base64_string):
     imgdata = base64.b64decode(str(base64_string))
@@ -44,4 +47,4 @@ def predictAPI():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port = 5800, ssl_context=(pathToCert,pathToKey))
